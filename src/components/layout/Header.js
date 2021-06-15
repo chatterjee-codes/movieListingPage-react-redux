de36209bb8d610f648c3fac8ef7f2ca3541c2754
codeForever033 <chatt.abhi033@gmail.com>
2021-06-16 01:26:44 +0530

    commit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,35 +8,39 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <Navbar collapseOnSelect expand='lg'>
-      <Navbar.Brand>
-        <Link to='/'>
-          <img src={logo} alt='' style={{ width: '120px' }} />
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-      <Navbar.Collapse id='responsive-navbar-nav'>
-        <Nav className='mr-auto'>
-          <Nav.Link href='#!'>Home</Nav.Link>
-          <Nav.Link href='#!'>TV Shows</Nav.Link>
-          <Nav.Link href='#!'>
-            <Link to='/movies'>Movies</Link>
-          </Nav.Link>
-          <Nav.Link href='#!'>My List</Nav.Link>
-        </Nav>
-        <br />
+    <div className='header'>
+      <Navbar collapseOnSelect expand='lg'>
+        <Navbar.Brand>
+          <Link to='/'>
+            <img src={logo} alt='' style={{ width: '120px' }} />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='mr-auto' id='header'>
+            <Nav.Link href='#'>
+              <Link to='/'>Home</Link>
+            </Nav.Link>
+            <Nav.Link href='#!'>TV Shows</Nav.Link>
+            <Nav.Link href='#!'>
+              <Link to='/movies'>Movies</Link>
+            </Nav.Link>
+            <Nav.Link href='#!'>My List</Nav.Link>
+          </Nav>
+          <br />
 
-        <Form className='d-flex'>
-          <FormControl
-            type='search'
-            placeholder='Search'
-            className='mr-2'
-            aria-label='Search'
-          />
-          <Button variant='outline-dark'>Search</Button>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
+          <Form className='d-flex'>
+            <FormControl
+              type='search'
+              placeholder='Search'
+              className='mr-2'
+              aria-label='Search'
+            />
+          </Form>
+          <Button className='btn-header'>Search</Button>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
   );
 };
 
